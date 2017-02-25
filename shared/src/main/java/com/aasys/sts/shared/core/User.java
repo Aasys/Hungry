63a4331aa59c0aea49b543585ca41db9405f7340
Aasys <mail@aasys.io>
2017-02-25 18:54:41 -0500

    Shared -  Core.User

---
+++ shared/src/main/java/com/aasys/sts/shared/core/User.java
@@ -0,0 +1,63 @@
+package com.aasys.sts.shared.core;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+/**
+ * Created by aasys on 2/25/2017.
+ */
+public class User implements IsSerializable {
+    private int userId;
+    private String name;
+    private String address;
+    private String phonenum;
+    private String email;
+    private String password;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhonenum() {
+        return phonenum;
+    }
+
+    public void setPhonenum(String phonenum) {
+        this.phonenum = phonenum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
