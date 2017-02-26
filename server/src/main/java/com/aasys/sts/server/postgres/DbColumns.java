2d7bc3c6b93a7c36663b49892fda5341b3690d43
Aasys <mail@aasys.io>
2017-02-25 19:54:52 -0500

    Server - DbColumns

---
+++ server/src/main/java/com/aasys/sts/server/postgres/DbColumns.java
@@ -0,0 +1,10 @@
+package com.aasys.sts.server.postgres;
+
+/**
+ * Created by aasys on 2/25/2017.
+ */
+public class DbColumns {
+    public static final String USERS = "users";
+    public static final String USERS_ID = "userid";
+    public static final String USERS_NAME = "name";
+}
